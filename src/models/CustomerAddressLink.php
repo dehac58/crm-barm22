ef57a5cb781f0bb17f973e3854e9d55afea19dc5
Rene <rene.oberkirch@ba-rm.de>
2024-11-26 13:42:08 +0100

    test

--- src/models/CustomerAddressLink.php
+++ src/models/CustomerAddressLink.php
@@ -1,11 +1,14 @@
 <?php
 
 class CustomerAddressLink {
+
+    public $customerAdressLinkID;
     public $customerId;
     public $addressId;
     private $isHeadOffice;
 
-    public function __construct($customerId, $addressId, $isHeadOffice) {
+    public function __construct($customerAdressLinkID, $customerId, $addressId, $isHeadOffice) {
+        $this->customerAdressLinkID = $customerAdressLinkID;
         $this->customerId = $customerId;
         $this->addressId = $addressId;
         $this->isHeadOffice = $isHeadOffice;
--- src/public/index.php
+++ src/public/index.php
@@ -21,45 +21,38 @@ $employeeController = new EmployeeController($employeeService);
 $customerService = new CustomerService($employeeService);
 $customerController = new CustomerController($customerService);
 
+$jwtMiddleware = new JwtMiddleware();
 // Apply JWT middleware to all routes except one
-/*
-$app->add(new JwtMiddleware())->add(function ($request, $handler) {
-    $route = $request->getAttribute('route');
-    $routeName = $route ? $route->getName() : '';
-
-    // Exclude the specific route from JWT middleware
-    if ($routeName === 'publicEndpoint') {
-        return $handler->handle($request);
-    }
-
-    return (new JwtMiddleware())($request, $handler);
-});
-*/
-
-$app->get('/employees', [$employeeController, 'getAllEmployees']);
-$app->get('/employees/{employeeId}', [$employeeController, 'getEmployee']);
-$app->post('/employees', [$employeeController, 'createEmployee']);
-$app->put('/employees/{employeeId}', [$employeeController, 'updateEmployee']);
-$app->delete('/employees/{employeeId}', [$employeeController, 'deleteEmployee']);
-
-$app->get('/customers', [$customerController, 'getAllCustomers']);
-$app->get('/customers/{customerId}', [$customerController, 'getCustomer']);
-$app->post('/customers', [$customerController, 'createCustomer']);
-$app->put('/customers/{customerId}', [$customerController, 'updateCustomer']);
-$app->delete('/customers/{customerId}', [$customerController, 'deleteCustomer']);
-
-$app->get('/customers/{customerId}/employees', [$customerController, 'getCustomerEmployees']);
-$app->get('/customers/{customerId}/employees/{employeeId}', [$customerController, 'getCustomerEmployee']);
-
-$app->get('/customers/{customerId}/addresses', [$customerController, 'getCustomerAddresses']);
-$app->post('/customers/{customerId}/addresses', [$customerController, 'createAddress']);
-$app->put('/customers/{customerId}/addresses/{addressId}', [$customerController, 'updateAddress']);
-$app->delete('/customers/{customerId}/addresses/{addressId}', [$customerController, 'deleteAddress']);
+// Define the login endpoint
+$app->post('/login', [$jwtMiddleware, 'login'])->setName('login');
 
 // Define the public endpoint
 $app->get('/public', function ($request, $response, $args) {
     $response->getBody()->write("This is a public endpoint");
     return $response;
 })->setName('publicEndpoint');
+
+// Apply JWT middleware to specific routes
+$app->group('', function (App $app) use ($employeeController, $customerController) {
+    $app->get('/employees', [$employeeController, 'getAllEmployees']);
+    $app->get('/employees/{employeeId}', [$employeeController, 'getEmployee']);
+    $app->post('/employees', [$employeeController, 'createEmployee']);
+    $app->put('/employees/{employeeId}', [$employeeController, 'updateEmployee']);
+    $app->delete('/employees/{employeeId}', [$employeeController, 'deleteEmployee']);
+
+    $app->get('/customers', [$customerController, 'getAllCustomers']);
+    $app->get('/customers/{customerId}', [$customerController, 'getCustomer']);
+    $app->post('/customers', [$customerController, 'createCustomer']);
+    $app->put('/customers/{customerId}', [$customerController, 'updateCustomer']);
+    $app->delete('/customers/{customerId}', [$customerController, 'deleteCustomer']);
+
+    $app->get('/customers/{customerId}/employees', [$customerController, 'getCustomerEmployees']);
+    $app->get('/customers/{customerId}/employees/{employeeId}', [$customerController, 'getCustomerEmployee']);
+
+    $app->get('/customers/{customerId}/addresses', [$customerController, 'getCustomerAddresses']);
+    $app->post('/customers/{customerId}/addresses', [$customerController, 'createAddress']);
+    $app->put('/customers/{customerId}/addresses/{addressId}', [$customerController, 'updateAddress']);
+    $app->delete('/customers/{customerId}/addresses/{addressId}', [$customerController, 'deleteAddress']);
+})->add(new JwtMiddleware());
 
 $app->run();
