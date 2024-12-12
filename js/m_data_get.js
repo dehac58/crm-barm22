7d078b80d052b15780da9270131ecab56717c691
Timon <Timon@vierke-sehnde.de>
2024-12-12 17:05:26 +0100

    Neue Funktion und Fixes

--- js/m_data_get.js
+++ js/m_data_get.js
@@ -3,25 +3,25 @@ var m_data_get = (function () {
     // Application object.
 const baseUrl = "https://dev.wappprojects.de/wiws22i/"
 
-    const m_data_get = {
-        getEmployees: (do_next_func) => { exchangeDataWithAjax(do_next_func, "employees", `${baseUrl}employees`); },
-        getEmployeeById: (do_next_func, employeeId) => { exchangeDataWithAjax(do_next_func, "employeeById", `${baseUrl}employees/${employeeId}`); },
-        getCustomers: (do_next_func) => {console.log('probieren geht über studieren'), exchangeDataWithAjax(do_next_func, "customers", `${baseUrl}customers`); },
-        getCustomerById: (do_next_func, customerId) => { exchangeDataWithAjax(do_next_func, "customerById", `${baseUrl}customers/${customerId}`); },
-        getEmployeesByCustomerId: (do_next_func, customerId) => { exchangeDataWithAjax(do_next_func, "employeesByCustomerId", `${baseUrl}customers/${customerId}/employees`); },
-        getEmployeeByIdByCustomerId: (do_next_func, customerId, employeeId) => { exchangeDataWithAjax(do_next_func, "employeeByIdByCustomerId", `${baseUrl}customers/${customerId}/employees/${employeeId}`); },
-        getAddressesByCustomerId: (do_next_func, customerId) => { exchangeDataWithAjax(do_next_func, "addressesByCustomerId", `${baseUrl}customers/${customerId}/addresses`); }
-    };
-
     // const m_data_get = {
-    //     getEmployees: (do_next_func) => { exchangeDataWithAjax(do_next_func, "employees", "./data/employees.json"); },
-    //     getEmployeeById: (do_next_func, eId) => { exchangeDataWithAjax(do_next_func, "employeeById", "./data/employees.json"); },
-    //     getCustomers: (do_next_func) => { exchangeDataWithAjax(do_next_func, "customers", "./data/customers.json"); },
-    //     getCustomerById: (do_next_func, cId) => { exchangeDataWithAjax(do_next_func, "customerById", "./data/customers.json"); },
-    //     getEmployeesByCustomerId: (do_next_func, cId) => { exchangeDataWithAjax(do_next_func, "employeesByCustomerId", "./data/employees.json"); },
-    //     getEmployeeByIdByCustomerId: (do_next_func) => { exchangeDataWithAjax(do_next_func, "employeeByIdByCustomerId", ""); },
-    //     getAddressesByCustomerId: (do_next_func) => { exchangeDataWithAjax(do_next_func, "addressesByCustomerId", ""); }
+    //     getEmployees: (do_next_func) => { exchangeDataWithAjax(do_next_func, "employees", `${baseUrl}employees`); },
+    //     getEmployeeById: (do_next_func, employeeId) => { exchangeDataWithAjax(do_next_func, "employeeById", `${baseUrl}employees/${employeeId}`); },
+    //     getCustomers: (do_next_func) => {console.log('probieren geht über studieren'), exchangeDataWithAjax(do_next_func, "customers", `${baseUrl}customers`); },
+    //     getCustomerById: (do_next_func, customerId) => { exchangeDataWithAjax(do_next_func, "customerById", `${baseUrl}customers/${customerId}`); },
+    //     getEmployeesByCustomerId: (do_next_func, customerId) => { exchangeDataWithAjax(do_next_func, "employeesByCustomerId", `${baseUrl}customers/${customerId}/employees`); },
+    //     getEmployeeByIdByCustomerId: (do_next_func, customerId, employeeId) => { exchangeDataWithAjax(do_next_func, "employeeByIdByCustomerId", `${baseUrl}customers/${customerId}/employees/${employeeId}`); },
+    //     getAddressesByCustomerId: (do_next_func, customerId) => { exchangeDataWithAjax(do_next_func, "addressesByCustomerId", `${baseUrl}customers/${customerId}/addresses`); }
     // };
+
+    const m_data_get = {
+        getEmployees: (do_next_func) => { exchangeDataWithAjax(do_next_func, "employees", "./data/employees.json"); },
+        getEmployeeById: (do_next_func, eId) => { exchangeDataWithAjax(do_next_func, "employeeById", "./data/employees.json"); },
+        getCustomers: (do_next_func) => { exchangeDataWithAjax(do_next_func, "customers", "./data/customers.json"); },
+        getCustomerById: (do_next_func, cId) => { exchangeDataWithAjax(do_next_func, "customerById", "./data/customers.json"); },
+        getEmployeesByCustomerId: (do_next_func, cId) => { exchangeDataWithAjax(do_next_func, "employeesByCustomerId", "./data/employees.json"); },
+        getEmployeeByIdByCustomerId: (do_next_func) => { exchangeDataWithAjax(do_next_func, "employeeByIdByCustomerId", ""); },
+        getAddressesByCustomerId: (do_next_func) => { exchangeDataWithAjax(do_next_func, "addressesByCustomerId", ""); }
+    };
 
     //************************************************
     // member variables
